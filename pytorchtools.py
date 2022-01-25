9a71268b6143eff15a80ec997241fb158c7a5f3c
Kento <48542394+szkn@users.noreply.github.com>
2022-01-25 17:27:47 +0900

    add feature : set compensate as true label

--- pytorchtools.py
+++ pytorchtools.py
@@ -3,7 +3,7 @@ import torch
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pth', trace_func=print):
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint', trace_func=print, label=""):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -26,6 +26,8 @@ class EarlyStopping:
         self.delta = delta
         self.path = path
         self.trace_func = trace_func
+        
+        self.label = label
     def __call__(self, val_loss, model):
 
         score = -val_loss
@@ -47,5 +49,5 @@ class EarlyStopping:
         '''Saves model when validation loss decrease.'''
         if self.verbose:
             self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
+        torch.save(model.state_dict(), self.path+ self.label +".pth")
         self.val_loss_min = val_loss
